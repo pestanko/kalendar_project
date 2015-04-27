5c33f8bf69aad279a997690e02221c0f72223819
Pepo48 <pepo48@gmail.com>
2015-04-27 17:51:22 +0200

    Added UserEditForm

---
+++ KalendarGUI/src/main/java/cz/muni/fi/pv168/project/zaostan/gui/forms/UsersEditForm.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pv168.project.zaostan.gui.forms;
+
+import javax.swing.*;
+
+/**
+ * Created by Pepo on 27.4.2015.
+ */
+public class UsersEditForm {
+    private JLabel labelFirstName;
+    private JLabel labelLastName;
+    private JTextField textFirstName;
+    private JTextField textLastName;
+    private JLabel labelUserName;
+    private JLabel labelEmail;
+    private JLabel labelMobileNumber;
+    private JLabel labelAddress;
+}
