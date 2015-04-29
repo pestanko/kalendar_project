86b1cd2f49aca5fd511dbe1c7e07a2f0cd36012e
Pepo48 <pepo48@gmail.com>
2015-04-29 08:09:04 +0200

    Added resource bundle translations

--- KalendarGUI/src/main/java/cz/muni/fi/pv168/project/zaostan/gui/forms/users/UsersEditForm.java
+++ KalendarGUI/src/main/java/cz/muni/fi/pv168/project/zaostan/gui/forms/users/UsersEditForm.java
@@ -15,4 +15,12 @@ public class UsersEditForm {
     private JLabel labelMobileNumber;
     private JLabel labelAddress;
     private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("UsersEditForm");
+        frame.setContentPane(new UsersEditForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
